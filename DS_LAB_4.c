a6b01c5eb0c290551b18cc033ba57cb9f54d0fba
SubhajitMishra04 <96262810+SubhajitMishra04@users.noreply.github.com>
2022-03-28 23:35:53 +0530

    Update DS_LAB_4.c

--- DS_LAB_4.c
+++ DS_LAB_4.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #define MAX 20
 
 struct node
@@ -14,10 +16,15 @@ struct node
 
 int main()
 {
-    int i = 0, n = 2;
+    int i = 0, count,j=0,result;
+    struct node temp;
+
+    printf("\nEnter no of Entries : ");
+    scanf("%d",&count);
 
     printf("\nEnter Phone details");
-    for(i=0;i<2;i++)
+    printf("\n************************\n");
+    for(i=0;i<count;i++)
     {
         printf("\nModel name : ");
         scanf("%s",p[i].name, MAX, stdin);
@@ -29,11 +36,65 @@ int main()
         scanf("%f", &p[i].price);
         printf("\n************************\n");
     }
-    for(i=0;i<2;i++)
+    for(i=0;i<count;i++)
+    {
+        printf("\nPhone:\n%s\n%s\n%d\n%f\n", p[i].name, p[i].brand, p[i].memory, p[i].price);
+    }
+
+
+    for (i = 0; i < count; i++)
+    {
+       for (j = 0; j < count; j++)
+       {
+           if (p[i].price<p[j].price)
+           {
+               temp=p[i];
+               p[i]=p[j];
+               p[j]=temp;     
+           }
+       
+       }
+
+    }
+printf("\n************************\n");
+printf("     After Sorting PRICE    ");
+printf("\n************************\n");
+
+    
+   for(i=0;i<count;i++)
+    {
+        printf("\nPhone:\n%s\n%s\n%d\n%f\n", p[i].name, p[i].brand, p[i].memory, p[i].price);
+    }
+
+    printf("\n************************\n");
+    printf("\n************************\n");
+    printf("     After Sorting BRAND    ");
+    printf("\n************************\n");
+    printf("\n************************\n");
+
+    for (i = 0; i < count; i++)
+    {
+       for (j = 0; j < count; j++)
+       {
+           result=strcmp(p[i].brand,p[j].brand);
+           if (result<0)
+           {
+               temp=p[i];
+               p[i]=p[j];
+               p[j]=temp;     
+           }
+       
+       }
+
+    }
+
+      for(i=0;i<count;i++)
     {
         printf("\nPhone:\n%s\n%s\n%d\n%f\n", p[i].name, p[i].brand, p[i].memory, p[i].price);
     }
 
 
 
+
 }
+
